c70a80e5bfdcfeaa172e99a41ee54bc4ad7b060d
mrpis2 <warhammerprogamer@gmail.com>
2023-12-06 22:42:18 +0700

    ChangeCurrency

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ChangeCurrency
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        float rate = 23000;
+        float USD;
+        Console.WriteLine("Nhap so luong USD: ");
+        USD = float.Parse(Console.ReadLine());
+        Console.WriteLine("Gia tri VND la: " + rate*USD);
+    }
+}
+}
